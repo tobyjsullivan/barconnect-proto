d2655e32ae4a15993a8ed9370dd56bc77af7df16
Toby Sullivan <tobyjsullivan@gmail.com>
2013-04-07 14:49:09 -0700

    Redesigned site to fully custom styling (no bootstrap used)

--- www/index.php
+++ www/index.php
@@ -3,35 +3,24 @@
 	<head>
 		<title>Tark Pub - BarConnect</title>
 		
-		<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-		<link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
 		<link href="css/barconnect.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<div class="container">
-			<div class="row">
-				<div class="span6">
+			<div class="header-wrapper">
+				<div class="header">
 					<h1>Tark Pub</h1>
 				</div>
-				<div class="span6">
-					<h2>BarConnect</h2>
-				</div>
 			</div>
-			<div class="row">
-				<div class="span4">
-					<a class="btn btn-large btn-menu" href="#">Drink Menu</a>
-				</div>
-				<div class="span4">
-					<a class="btn btn-large btn-menu" href="#">Call Waitress</a>
-				</div>
-				<div class="span4">
-					<a class="btn btn-large btn-menu" href="#">About/Information</a>
-				</div>
+			<div class="menu-wrapper">
+				<ul class="menu">
+					<li><a href="#">Drink Menu</a></li>
+					<li><a href="#">Call Waitress</a></li>
+					<li><a href="#">About Bar Connect</a></li>
+				</ul>
 			</div>
-			<div class="row">
-				<div class="span6">
-					&copy; Copyright 2013 Tark Engineering
-				</div>
+			<div class="footer-wrapper">
+				<div class="footer">&copy; Copyright 2013 Tark Engineering</div>
 			</div>
 		</div>
 	</body>
