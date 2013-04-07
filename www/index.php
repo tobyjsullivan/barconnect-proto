546b684252a203de8d4a1a11b675944f00beb34b
Toby Sullivan <tobyjsullivan@gmail.com>
2013-04-07 13:59:52 -0700

    first commit

---
+++ www/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tark Pub - BarConnect</title>
+		
+		<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+		<link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
+		<link href="css/barconnect.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="span6">
+					<h1>Tark Pub</h1>
+				</div>
+				<div class="span6">
+					<h2>BarConnect</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="span4">
+					<a class="btn btn-large btn-menu" href="#">Drink Menu</a>
+				</div>
+				<div class="span4">
+					<a class="btn btn-large btn-menu" href="#">Call Waitress</a>
+				</div>
+				<div class="span4">
+					<a class="btn btn-large btn-menu" href="#">About/Information</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="span6">
+					&copy; Copyright 2013 Tark Engineering
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
